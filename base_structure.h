0f59fc042c3b366b13db1b38136a4b6c8715c3df
roket64 <1p41i4y1@gmail.com>
2022-11-08 01:03:47 +0900

    uwu

--- base_structure.h
+++ base_structure.h
@@ -3,6 +3,7 @@
 
 #include <type_traits>
 #include <concepts>
+#include <memory>
 
 template<class T>
 concept mutable_integer = std::is_integral_v<T> && !std::is_const_v<T>;
@@ -10,6 +11,9 @@ concept mutable_integer = std::is_integral_v<T> && !std::is_const_v<T>;
 template<class NodeType = int> requires mutable_integer<NodeType>
 class base_edge {
 public:
+    /*
+     * declaration of types in base_edge
+     */
     typedef NodeType node_t;
     typedef const node_t const_node_t;
 
@@ -19,34 +23,61 @@ public:
     typedef node_t &node_t_ref;
     typedef const node_t &const_node_t_ref;
 
+    /*
+     * constructors
+     */
     base_edge() :
             to_(-1),
             from_(-1) {};
 
-    explicit base_edge(const node_t to) :
+    explicit base_edge(const_node_t to) :
             to_(to),
-            from_(-1) {}
+            from_(-1) {};
 
-    base_edge(const node_t to, const node_t from) :
+    base_edge(const_node_t from, const_node_t to) :
             to_(to),
-            from_(from) {}
+            from_(from) {};
 
-    inline const_node_t_ref to() const {
+    /*
+     * getter functions
+     */
+    const_node_t_ref to() const {
         return this->to_;
     }
 
-    inline const_node_t_ref from() const {
+    const_node_t_ref from() const {
         return this->from_;
     }
 
 private:
-    const_node_t to_;
     const_node_t from_;
+    const_node_t to_;
 };
 
 template<class NodeType = int, class FlowType = int> requires mutable_integer<FlowType>
-class flow_edge : base_edge<NodeType> {
+class flow_edge : public base_edge<NodeType> {
 public:
+    /*
+     * reuse of types from base_edge
+     */
+    typedef typename
+    base_edge<NodeType>::node_t node_t;
+    typedef typename
+    base_edge<NodeType>::const_node_t const_node_t;
+
+    typedef typename
+    base_edge<NodeType>::node_t_ptr node_t_ptr;
+    typedef typename
+    base_edge<NodeType>::const_node_t_ptr const_node_t_ptr;
+
+    typedef typename
+    base_edge<NodeType>::node_t_ref node_t_ref;
+    typedef typename
+    base_edge<NodeType>::const_node_t_ref const_node_t_ref;
+
+    /*
+     * declaration of types in flow_edge
+     */
     typedef FlowType flow_t;
     typedef const flow_t const_flow_t;
 
@@ -56,26 +87,18 @@ public:
     typedef flow_t &flow_t_ref;
     typedef const flow_t &const_flow_t_ref;
 
-    typedef flow_edge<NodeType, FlowType> edge;
-    typedef const flow_edge<NodeType, FlowType> const_edge;
+    typedef flow_edge<node_t, flow_t> edge;
+    typedef const flow_edge<node_t, flow_t> const_edge;
 
-    typedef flow_edge<NodeType, FlowType> *edge_ptr;
-    typedef const flow_edge<NodeType, FlowType> *const_edge_ptr;
+    typedef flow_edge<node_t, flow_t> *edge_ptr;
+    typedef const flow_edge<node_t, flow_t> *const_edge_ptr;
 
-    typedef flow_edge<NodeType, FlowType> &edge_ref;
-    typedef const flow_edge<NodeType, FlowType> &const_edge_ref;
+    typedef flow_edge<node_t, flow_t> &edge_ref;
+    typedef const flow_edge<node_t, flow_t> &const_edge_ref;
 
-    typedef typename
-    base_edge<NodeType>::node_t node_t;
-    typedef typename
-    base_edge<NodeType>::const_node_t const_node_t;
-
-    typedef typename
-    base_edge<NodeType>::node_t_ptr node_t_ptr;
-
-    typedef typename
-    base_edge<NodeType>::node_t_ref node_t_ref;
-
+    /*
+     *  constructors
+     */
     flow_edge() :
             base_edge<node_t>(),
             capacity_(0),
@@ -88,53 +111,59 @@ public:
             flow_(0),
             reversal_edge_pointer_(nullptr) {};
 
-    flow_edge(const_node_t to,
-              const_node_t from) :
-            base_edge<node_t>(to, from),
+    flow_edge(const_node_t from,
+              const_node_t to) :
+            base_edge<node_t>(from, to),
             capacity_(0),
             flow_(0),
             reversal_edge_pointer_(nullptr) {};
 
-    flow_edge(const_node_t to,
-              const_node_t from,
+    flow_edge(const_node_t from,
+              const_node_t to,
               const_flow_t capacity) :
-            base_edge<node_t>(to, from),
-            capacity_(0),
+            base_edge<node_t>(from, to),
+            capacity_(capacity),
             flow_(0),
             reversal_edge_pointer_(nullptr) {};
 
-    flow_edge(const_node_t to,
-              const_node_t from,
+    flow_edge(const_node_t from,
+              const_node_t to,
               const_flow_t capacity,
               edge_ptr reversal) :
-            base_edge<NodeType>(to, from),
+            base_edge<node_t>(from, to),
             capacity_(capacity),
             flow_(0),
             reversal_edge_pointer_(reversal) {};
 
-    inline const_flow_t_ref capacity() const {
+    /*
+     *  getter functions
+     */
+    const_flow_t_ref capacity() const {
         return this->capacity_;
     }
 
-    inline const_flow_t_ref flow() const {
+    const_flow_t_ref flow() const {
         return this->flow_;
     }
 
-    inline const_flow_t_ref spare() const {
+    const_flow_t_ref spare() const {
         return this->capacity_ - this->flow_;
     }
 
+    /*
+     * setter functions
+     */
     void set_capacity(const_flow_t amount) {
         this->capacity_ = amount;
+    }
+
+    void set_reversal(edge_ptr reversal) {
+        this->reversal_edge_pointer_ = reversal;
     }
 
     void push_flow(const_flow_t amount) {
         this->reversal_edge_pointer_->flow_ -= amount;
         this->flow_ += amount;
-    }
-
-    void set_reversal(edge_ptr reversal) {
-        this->reversal_edge_pointer_ = reversal;
     }
 
 private:
--- dinic.h
+++ dinic.h
@@ -2,22 +2,65 @@
 #define GRAPH_ALGORITHM_DINIC_H
 
 #include <iostream>
-#include <cassert>
 #include <vector>
+#include <queue>
 #include <algorithm>
 #include "base_structure.h"
 
 template<class NodeType = int, class FlowType = int>
-class dinic : flow_edge<NodeType, FlowType> {
+class dinic : public flow_edge<NodeType, FlowType> {
 public:
-    typedef flow_edge<NodeType, FlowType> edge;
-    typedef flow_edge<NodeType, FlowType> *edge_pointer;
-    typedef flow_edge<NodeType, FlowType> &edge_reference;
+    typedef flow_edge<NodeType, FlowType> *f_edge_pointer;
+    typedef flow_edge<NodeType, FlowType> &f_edge_reference;
+
+    /*
+     * reuse of types from flow_edge
+     */
+    typedef typename
+    flow_edge<NodeType>::node_t node_t;
+    typedef typename
+    flow_edge<node_t>::const_node_t const_node_t;
+
+    typedef typename
+    flow_edge<node_t>::flow_t flow_t;
+    typedef typename
+    flow_edge<node_t>::const_flow_t const_flow_t;
+
+    typedef typename
+    flow_edge<node_t, flow_t>::edge f_edge;
+    typedef typename
+    flow_edge<node_t, flow_t>::const_edge const_f_edge;
+
+    typedef typename
+    flow_edge<node_t, flow_t>::edge_ptr f_edge_ptr;
+    typedef typename
+    flow_edge<node_t, flow_t>::const_edge_ptr const_f_edge_ptr;
 
 private:
     std::vector<int> level_;
     std::vector<int> work_;
-    std::vector<edge> adj_;
+    std::vector<f_edge> adj_;
+    std::vector<f_edge_pointer> adj_ptr_;
+
+    bool can_reach(node_t source, node_t sink) {
+        std::fill(level_.begin(), level_.end(), -1);
+        std::fill(work_.begin(), work_.end(), 0);
+        std::queue<node_t> queue;
+
+        while (!queue.empty()) {
+            node_t cur_ = queue.front();
+            queue.pop();
+            if (cur_ == sink) return true;
+            for (auto &edge_: adj_[cur_]) {
+                const_node_t next_ = edge_.to();
+                if (level_[next_] == -1
+                    && edge_.spare() > 0) {
+                    queue.push(next_);
+                    level_[next_] = level_[cur_] + 1;
+                }
+            }
+        }
+    }
 };
 
 #endif //GRAPH_ALGORITHM_DINIC_H
