c610e477550eefb1e659f9fdf38fd63b86c01067
roket64 <1p41i4y1@gmail.com>
2022-11-08 15:39:40 +0900

    uwu

--- main.cpp
+++ main.cpp
@@ -6,33 +6,35 @@
 std::vector<int> work(5);
 std::vector<int> level(5);
 
-/*
-int dfs(int u, int t, int f, std::vector<std::vector<flow_edge<>::edge_ptr>> &adj) {
-    if (u == t) return f;
-    for (auto &i = work[u]; i < adj[u].size(); ++i) {
-        auto next_ = adj[u][i];
-        std::cout << next_->from() << " -> " << next_->to() << ": ";
-        std::cout << next_->flow() << " / " << next_->capacity() << ", spare: " << next_->spare() << std::endl;
-        if (level[next_->to()] == level[next_->from()] + 1 && next_->spare() > 0) {
-            if (flow_edge<>::flow_t ret = dfs(next_->to(), t, std::min(f, next_->spare()), adj)) {
-                next_->push_flow(ret);
-                return ret;
-            }
-        }
-    }
-    return 0;
+template<class T>
+constexpr std::string_view type_name() {
+#ifdef __clang__
+    std::string_view p = __PRETTY_FUNCTION__;
+    return {p.data() + 34, p.size() - 34 - 1};
+#elif defined(__GNUC__)
+    std::string_view p = __PRETTY_FUNCTION__;
+#  if __cplusplus < 201402
+    return string_view(p.data() + 36, p.size() - 36 - 1);
+#  else
+    return std::string_view(p.data() + 49, p.find(';', 49) - 49);
+#  endif
+#elif defined(_MSC_VER)
+    string_view p = __FUNCSIG__;
+    return string_view(p.data() + 84, p.size() - 84 - 7);
+#endif
 }
-/**/
 
 int dfs(int u, int t, int f, std::vector<std::vector<flow_edge<>>> &adj) {
     if (u == t) return f;
     for (auto &i = work[u]; i < adj[u].size(); ++i) {
-        auto next_ = adj[u][i];
-        std::cout << next_.from() << " -> " << next_.to() << ": ";
-        std::cout << next_.flow() << " / " << next_.capacity() << ", spare: " << next_.spare() << std::endl;
-        if (level[next_.to()] == level[next_.from()] + 1 && next_.spare() > 0) {
-            if (flow_edge<>::flow_t ret = dfs(next_.to(), t, std::min(f, next_.spare()), adj)) {
-                next_.push_flow(ret);
+        auto next_ = &adj[u][i];
+        std::cout << next_->from() << " -> " << next_->to() << ": ";
+        std::cout << next_->flow() << " / " << next_->capacity() << ", spare: " << next_->spare() << std::endl;
+        if (level[next_->to()] == level[next_->from()] + 1 && next_->spare() > 0) {
+            if (auto ret = dfs(next_->to(), t, std::min(f, next_->spare()), adj)) {
+                next_->push_flow(ret);
+                std::cout << "pushed: " << ret << ", ";
+                std::cout << next_->flow() << " / " << next_->capacity() << std::endl;
                 return ret;
             }
         }
@@ -85,8 +87,23 @@ int main() {
         }
     }
 
-    auto flow = dfs(1, 4, 0x3f3f3f3f, adj);
-    std::cout << flow << std::endl;
+    int ans = 0;
+    while (auto flow = dfs(1, 4, 0x3f3f3f3f, adj)) {
+        ans += flow;
+    }
+    std::cout << ans << std::endl;
+
+//    1 -> 2: 0 / 10, spare: 10
+//    2 -> 4: 0 / 10, spare: 10
+//    pushed: 10, 10 / 10
+//    pushed: 10, 10 / 10
+//    1 -> 2: 10 / 10, spare: 0
+//    1 -> 3: 0 / 10, spare: 10
+//    3 -> 4: 0 / 10, spare: 10
+//    pushed: 10, 10 / 10
+//    pushed: 10, 10 / 10
+//    1 -> 3: 10 / 10, spare: 0
+//    20
 
     return 0;
 }
