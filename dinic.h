674a3f404b2edfc1ef9f6dcb93cc2c692701bd20
roket64 <1p41i4y1@gmail.com>
2022-11-16 23:39:22 +0900

    included the basic component of the graph and constructors

--- dinic.h
+++ dinic.h
@@ -1,18 +1,48 @@
 #ifndef GRAPH_ALGORITHM_DINIC_H
 #define GRAPH_ALGORITHM_DINIC_H
 
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <limits>
+#include <queue> // to use of BFS
+#include <algorithm> // to use of std::min
+#include <limits> // to use of std::numeric_limits<>::max()
 #include "base_structure.h"
 
-template<int MAX_V, class Node = int, class Flow = int>
-class Dinic : public FlowEdge<Node, Flow> {
+template<class Node = int, class Flow = int>
+class Dinic {
 public:
+    Dinic() :
+            source_(0),
+            sink_(0) {};
+
+    Dinic(Node max_v) :
+            source_(0),
+            sink_(0) {
+        level_.resize(max_v + 1);
+        work_.resize(max_v + 1);
+        adj_.resize(max_v + 1);
+    }
+
+    Dinic(Node source,
+          Node sink) :
+            source_(source),
+            sink_(sink) {};
+
+    Dinic(Node source,
+          Node sink,
+          Node max_v) :
+            source_(source),
+            sink_(sink) {
+        level_.resize(max_v + 1);
+        work_.resize(max_v + 1);
+        adj_.resize(max_v + 1);
+    }
+
 private:
-    int level[MAX_V];
+    const Flow INF = std::numeric_limits<Flow>::max();
+    Node source_, sink_;
+
+    std::vector<Node> level_;
+    std::vector<Node> work_;
+    std::vector<std::vector<FlowEdge<> *>> adj_;
 };
 
 #endif //GRAPH_ALGORITHM_DINIC_H
